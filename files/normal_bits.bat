693ae9438098b4e15716e0a3229592f7c9207951
id7mgh <68156126+id7mgh@users.noreply.github.com>
2023-02-12 22:23:26 +0300

    Update normal_bits.bat

--- files/normal_bits.bat
+++ files/normal_bits.bat
@@ -1,8 +1,9 @@
+@echo off
 for /f "tokens=3" %%a in ('sc queryex "BITS" ^| findstr "PID"') do (set pid=%%a)
-wmic process where ProcessId=%pid% CALL setpriority "realtime" 
+wmic process where ProcessId=%pid% CALL setpriority "realtime"  >nul
 
 :bitsendless
-PING -n 5 127.0.0.1 
-sc query BITS | find /I "STATE" | find "STOPPED"
-sc start BITS
+PING -n 5 127.0.0.1 >nul
+sc query BITS | find /I "STATE" | find "STOPPED" >nul
+sc start BITS >nul
 goto bitsendless
