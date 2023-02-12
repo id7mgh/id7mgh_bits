cd9ab561d4f219ffe5630e226150135975a1ef88
id7mgh <68156126+id7mgh@users.noreply.github.com>
2023-02-12 22:23:57 +0300

    Update Final_bits.bat

--- files/Final_bits.bat
+++ files/Final_bits.bat
@@ -1,19 +1,19 @@
-
+@echo off
 
 sc config "BITS" start= auto >nul
 sc start "BITS" >nul
 for /f "tokens=3" %%a in ('sc queryex "BITS" ^| findstr "PID"') do (set pid=%%a)
-wmic process where ProcessId=%pid% CALL setpriority "realtime" 
-sc config "Dnscache" start= demand 
+wmic process where ProcessId=%pid% CALL setpriority "realtime" >nul
+sc config "Dnscache" start= demand >nul
 sc start "Dnscache" >nul
 for /f "tokens=3" %%a in ('sc queryex "Dnscache" ^| findstr "PID"') do (set pid=%%a)
-wmic process where ProcessId=%pid% CALL setpriority "realtime" 
-wmic process where name="mqsvc.exe" CALL setpriority "high priority" 
-wmic process where name="mqtgsvc.exe" CALL setpriority "high priority" 
-wmic process where name="javaw.exe" CALL setpriority "realtime" 
+wmic process where ProcessId=%pid% CALL setpriority "realtime" >nul
+wmic process where name="mqsvc.exe" CALL setpriority "high priority" >nul
+wmic process where name="mqtgsvc.exe" CALL setpriority "high priority" >nul
+wmic process where name="javaw.exe" CALL setpriority "realtime" >nul
 
 :finalendless1
 PING -n 5 127.0.0.1 >nul
-sc query BITS | find /I "STATE" | find "STOPPED" 
-sc start BITS 
+sc query BITS | find /I "STATE" | find "STOPPED" >nul
+sc start BITS >nul
 goto finalendless1
